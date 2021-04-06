451c2392cbd0c8081d523e1305245cd86f47f47b
guigui-bubu <guillaumebuaud@yahoo.fr>
2021-04-06 12:58:40 +0200

    first

---
+++ src/components/Footer.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+
+function Landing() {
+  return (
+    <footer>
+        <div className="footer-container">
+            <p>Projet réalisé par Guillaume Buaud - 2021</p>
+            <p>Les icônes Wolverine, Iron-man, Spider-man, Batman sont prises sur le site iconFinder.com</p>
+        </div>
+    </footer>
+  );
+}
+
+export default Landing;
