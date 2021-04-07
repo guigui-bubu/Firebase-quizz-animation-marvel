93ae389ba3ae415972b4e6bd530fb602f06f46dc
guigui-bubu <guillaumebuaud@yahoo.fr>
2021-04-07 11:57:06 +0200

    progresse bar, quizz state component

--- src/components/Quiz.js
+++ src/components/Quiz.js
@@ -1,17 +1,72 @@
 import React, { Component} from "react";
+import Levels from "./Levels";
+import ProgressBar from "./ProgressBar";
+import {QuizMarvel} from "./QuizMarvel";
+
 
 
 class Quizz extends Component {
 
+  state = {
+    levelNames : ["debutant","confirme","expert"],
+    quizLevel : 0,
+    maxQuestions : 10,
+    storedQuestions : [],
+    question : null,
+    options: [],
+    idQuestion : 0
+  }
+
+  loadQuestions = quizz => {
+     const fetchedArrayQuiz = QuizMarvel[0].quizz[quizz];
+     if(fetchedArrayQuiz.length >= this.state.maxQuestions){
+
+      const newArray = fetchedArrayQuiz.map(({answer, ...keepRest})=> keepRest); // nouveau tableau sans la réponse de la question 
+
+      this.setState({   // Mettre a jour le state
+        storedQuestions : newArray 
+      })
+
+     } else {
+       console.log("Pas assez de questions !!")
+     }
+  }
+
+ componentDidMount () {
+    this.loadQuestions(this.state.levelNames[this.state.quizLevel])
+ }
+
+ componentDidUpdate(prevProps, prevState) {
+   if (this.state.storedQuestions !== prevState.storedQuestions) {
+       
+        this.setState({
+          question: this.state.storedQuestions[this.state.idQuestion].question,
+          options: this.state.storedQuestions[this.state.idQuestion].options
+        })
+   }
+ }
+
   render () {
 
-    const { pseudo } = this.props.userData;
+    const displayOptions = this.state.options.map((option, index)=>{
+          return (
+            <p key={index} className="answerOptions">{option}</p>
+          )
+    })
 
 
     return (
       <div>
-       <h2>pseudo : {pseudo}</h2>
+       
+       <Levels />
+       <ProgressBar />
+
+       <h2>{this.state.question}</h2>
+       {displayOptions}
+       <button className="btnSubmit">Suivant</button>
+
       </div>
+
       );
   }
 }
